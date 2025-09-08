c1d3396347e0c2037712dca19038bc83fe49e37a
Thiago Gonçalves Salomé <thiago.salome@inter.co>
2025-09-08 08:36:07 -0300

    feat: expense app

--- expense-app/src/app.service.ts
+++ expense-app/src/app.service.ts
@@ -1,3 +1,13 @@
 import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {}
+
+/*
+
+Options to configure Postgres
+
+  1 - Install Postgres on local machine (the installation is more complicated)
+  2 - Host Postgres in cloud (easiest option)
+
+  Heroku doesn't have a free plan anymore. Because of that a suggestion is use Docker.
+*/
