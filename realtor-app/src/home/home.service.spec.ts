59e8b057428f6492fb0a3167d9be87c1e2df80d1
Thiago Gonçalves Salomé <thiago.salome@inter.co>
2025-09-24 08:36:22 -0300

    90. The Structure of a Test File

--- realtor-app/src/home/home.service.spec.ts
+++ realtor-app/src/home/home.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HomeService } from './home.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+// Nestjs uses Jest for test
 
 describe('HomeService', () => {
   let service: HomeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HomeService],
+      providers: [HomeService, PrismaService],
     }).compile();
 
     service = module.get<HomeService>(HomeService);
