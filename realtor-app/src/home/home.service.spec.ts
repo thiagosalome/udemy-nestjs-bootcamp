a5d55cf18cd8664c0947e7385afefa415a49e1b8
Thiago Gonçalves Salomé <thiago.salome@inter.co>
2025-09-26 09:20:35 -0300

    93. Testing the Create Service

--- realtor-app/src/home/home.service.spec.ts
+++ realtor-app/src/home/home.service.spec.ts
@@ -7,16 +7,16 @@ import { NotFoundException } from '@nestjs/common';
 // Nestjs uses Jest for test
 const mockGetHomes = [
   {
-    id: 2,
+    id: 1,
     address: '111 Axe Ave',
     city: 'Dallas',
     price: 500000,
     image: 'img5',
-    numberOfBedrooms: 4,
-    numberOfBathrooms: 5,
+    number_of_bedrooms: 4,
+    number_of_bathrooms: 5,
     listedDate: '2025-09-17T12:01:27.743Z',
     landSize: 3500,
-    propertyType: 'CONDO',
+    property_type: PropertyType.CONDO,
     images: [
       {
         url: 'src1',
@@ -25,7 +25,31 @@ const mockGetHomes = [
   },
 ];
 
-describe('HomeService', () => {
+const mockHome = {
+  id: 1,
+  address: '111 Axe Ave',
+  city: 'Dallas',
+  price: 500000,
+  image: 'img5',
+  number_of_bedrooms: 4,
+  number_of_bathrooms: 5,
+  listedDate: '2025-09-17T12:01:27.743Z',
+  landSize: 3500,
+  property_type: PropertyType.CONDO,
+};
+
+const mockImages = [
+  {
+    id: 1,
+    url: 'src1',
+  },
+  {
+    id: 2,
+    url: 'src2',
+  },
+];
+
+const mockCreateHomeParams = describe('HomeService', () => {
   let service: HomeService;
   let prismaService: PrismaService;
 
@@ -39,8 +63,13 @@ describe('HomeService', () => {
           useValue: {
             // Mocking home object
             home: {
-              // Mocking findMany function using jest.fn(). The mockReturn valeu I use to define the function return
+              // Mocking findMany function using jest.fn(). The mockReturn value I use to define the function return
               findMany: jest.fn().mockReturnValue(mockGetHomes),
+              // Mocking create function
+              create: jest.fn().mockReturnValue(mockHome),
+            },
+            image: {
+              createMany: jest.fn().mockReturnValue(mockImages),
             },
           },
         },
@@ -101,6 +130,65 @@ describe('HomeService', () => {
       await expect(service.getHomes(filters)).rejects.toThrowError(
         NotFoundException,
       );
+    });
+  });
+
+  describe('createHome', () => {
+    const mockCreateHomeParams = {
+      address: '111 Axe Ave',
+      numberOfBedrooms: 2,
+      numberOfBathrooms: 2,
+      city: 'Vancouver',
+      price: 3000000,
+      landSize: 444,
+      propertyType: PropertyType.RESIDENTIAL,
+      images: [
+        {
+          url: 'src1',
+        },
+      ],
+    };
+
+    it('should call prisma home.create with the correct payload', async () => {
+      const mockCreateHome = jest.fn().mockReturnValue(mockHome);
+
+      jest
+        .spyOn(prismaService.home, 'create')
+        .mockImplementation(mockCreateHome);
+
+      await service.createHome(mockCreateHomeParams, 5);
+
+      expect(mockCreateHome).toBeCalledWith({
+        data: {
+          address: '111 Axe Ave',
+          number_of_bedrooms: 2,
+          number_of_bathrooms: 2,
+          city: 'Vancouver',
+          price: 3000000,
+          land_size: 444,
+          property_type: PropertyType.RESIDENTIAL,
+          realtor_id: 5,
+        },
+      });
+    });
+
+    it('should call prisma image.createMany with the correct payload', async () => {
+      const mockCreateManyImages = jest.fn().mockReturnValue(mockImages);
+
+      jest
+        .spyOn(prismaService.image, 'createMany')
+        .mockImplementation(mockCreateManyImages);
+
+      await service.createHome(mockCreateHomeParams, 5);
+
+      expect(mockCreateManyImages).toBeCalledWith({
+        data: [
+          {
+            url: 'src1',
+            home_id: 1,
+          },
+        ],
+      });
     });
   });
 });
