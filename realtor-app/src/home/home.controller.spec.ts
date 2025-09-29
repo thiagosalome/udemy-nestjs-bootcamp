cd90301fdb18c045043268330538950a2e646344
Thiago Gonçalves Salomé <thiago.salome@inter.co>
2025-09-29 09:03:43 -0300

    96. More Controller Tests

--- realtor-app/src/home/home.controller.spec.ts
+++ realtor-app/src/home/home.controller.spec.ts
@@ -2,6 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HomeController } from './home.controller';
 import { HomeService } from './home.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UnauthorizedException } from '@nestjs/common';
+import { PropertyType } from '@prisma/client';
+
+const mockUser = {
+  id: 53,
+  name: 'Laith',
+  email: 'laith@laith.laith',
+  phone: '555 555 5555',
+};
+
+const mockHome = {
+  id: 1,
+  address: '111 Axe Ave',
+  city: 'Dallas',
+  price: 500000,
+  image: 'img5',
+  number_of_bedrooms: 4,
+  number_of_bathrooms: 5,
+  listedDate: '2025-09-17T12:01:27.743Z',
+  landSize: 3500,
+  property_type: PropertyType.CONDO,
+};
 
 describe('HomeController', () => {
   let controller: HomeController;
@@ -17,6 +39,10 @@ describe('HomeController', () => {
           useValue: {
             // Mocking getHomes method
             getHomes: jest.fn().mockReturnValue([]),
+            // Mocking getRealtorByHomeId method
+            getRealtorByHomeId: jest.fn().mockReturnValue(mockUser),
+            // Mocking updateHomeById
+            updateHomeById: jest.fn().mockReturnValue(mockHome),
           },
         },
         PrismaService,
@@ -29,18 +55,50 @@ describe('HomeController', () => {
 
   describe('getHomes', () => {
     it('should construct filter object correctly', async () => {
+      // Mocking getHomes method from home service
       const mockGetHomes = jest.fn().mockReturnValue([]);
-
       jest.spyOn(homeService, 'getHomes').mockImplementation(mockGetHomes);
 
+      // Calling getHomes from controller
       await controller.getHomes('Toronto', '1500000');
 
+      // Checking if mockGetHomes method was called with the correct params
       expect(mockGetHomes).toBeCalledWith({
         city: 'Toronto',
         price: {
           gte: 1500000,
         },
       });
+    });
+  });
+
+  describe('updateHome', () => {
+    const mockUserInfo = {
+      name: 'Laith',
+      id: 30,
+      iat: 1,
+      exp: 2,
+    };
+
+    const mockUpdateHomeParams = {
+      address: '111 Axe Ave',
+      numberOfBedrooms: 2,
+      numberOfBathrooms: 2,
+      city: 'Vancouver',
+      price: 3000000,
+      landSize: 444,
+      propertyType: PropertyType.RESIDENTIAL,
+      images: [
+        {
+          url: 'src1',
+        },
+      ],
+    };
+
+    it('should throw unauth error if realtor did not creat home', async () => {
+      await expect(
+        controller.updateHome(5, mockUpdateHomeParams, mockUserInfo),
+      ).rejects.toThrowError(UnauthorizedException);
     });
   });
 });
